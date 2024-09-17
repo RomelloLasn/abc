9860c0db9f6dca89a02c3b0902aadde5ca824bce
Gen Vikat <gen@veebisepad.ee>
2024-09-17 21:38:09 +0300

    Protect the weather endpoint with auth middleware

--- routes/weatherRoutes.js
+++ routes/weatherRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { getWeather } from '../controllers/weatherController.js';
 import { apiLimiter } from '../middleware/rateLimiter.js';
+import { authenticateToken } from '../middleware/auth.js';
 
 const router = express.Router();
 
-router.get('/:city', apiLimiter, getWeather);
+router.get('/:city', apiLimiter, authenticateToken, getWeather);
 
 export default router;
